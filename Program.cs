4d29ad65b4ee75f001599be249ce2ab0f911a053
AntonAstashkin <astashkinanton1@gmail.com>
2023-09-30 16:32:21 +0300

    First and final version of this program

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿namespace FibonacciNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many Fibonacci numbers you want to generate:");
+            int n = int.Parse(Console.ReadLine());
+
+            int[] numbers = new int[n];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+
+                if (i == 0 || i == 1)
+                {
+                    numbers[i] = 1;
+                }
+                else
+                {
+                    numbers[i] = numbers[i - 1] + numbers[i - 2];
+                }
+                Console.Write(numbers[i] + " ");
+            }
+        }
+    }
+}
